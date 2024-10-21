9af490c08d886432d86ad96e9cfe4352189c691a
Digvijaysinh160 <digvijaythorve95@gmail.com>
2024-10-21 10:05:46 +0530

    Create init_db.py

---
+++ init_db.py
@@ -0,0 +1,17 @@
+vimport sqlite3
+
+
+if __name__ == '__main__':
+
+    conn = sqlite3.connect('stock.db')
+
+    c = conn.cursor()
+
+
+    c.execute("""CREATE TABLE stock (
+                name text,
+                quantity integer,
+                cost integer
+                ) """)
+
+    conn.commit()
